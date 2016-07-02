6193f5017117c8ffffde9dd6d199c19ea107b84e
webjust <604854119@qq.com>
2016-07-02 21:35:46 +0800

    习题

--- learn python the hard way 4/ex1.py
+++ learn python the hard way 4/ex1.py
@@ -1,3 +1,5 @@
+#coding:utf-8
+print "ÖÐÎÄ×Ö·û"
 print "hello world!"
 print "hello again"
 print "i like typing this"
---
+++ learn python the hard way 4/ex2.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this."	# and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+
+# print "This won't run."
+
+print "This will run."
+
+# 这是注释行
+print "#后是注释内容，你还可以用注释的方法将这段代码临时禁用"
