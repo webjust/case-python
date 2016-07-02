269f2cd847c466bf0286c6b72e34ad3c72e2df0f
webjust <604854119@qq.com>
2016-07-02 22:30:54 +0800

    学习代码:

---
+++ learn python the hard way 4/ex3.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+
+# + - * / < > >= <=
+
+print "I will now count my chickens:"
+print "Hens", 25+30/6
+print "Roosters", 100-25*3%4
+print "Now I will count the eggs:"
+print 3+2+1-5+4%2-1/4+6
+print 1.0/4
+print 1/4
+print "Is it true that 3+2<5-7?"
+print 3+(2<5)-7
+print 3+2<5-7
+print (2<5)+1
+print (2>5)+1
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is is greater?", 5>-2
+print "Is it greate or equal?", 5>=-2
+print "Is is less or equal?", 5<=-2
+
+'''
+运行结果
+I will now count my chickens:
+Hens 30
+Roosters 97
+Now I will count the eggs:
+7
+0.25
+0
+Is it true that 3+2<5-7?
+-3
+False
+2
+1
+What is 3+2? 5
+What is 5-7? -2
+Oh, that's why it's False.
+How about some more.
+Is is greater? True
+Is it greate or equal? True
+Is is less or equal? False
+'''
